5f5f48ef08b4c7766c9967beab5188aa09b1f849
lowizetech <lowize.tech@gmail.com>
2025-09-28 21:18:38 +0800

    Updated configuration. Initialized tailwindcss and preprocessors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: { port: 3000, strictPort: true },
+  resolve: {
+    alias: {
+        '@assets': path.resolve(__dirname, './src/lib/assets'),
+        '@components': path.resolve(__dirname, './src/lib/components'),
+    }
+  }
+});
