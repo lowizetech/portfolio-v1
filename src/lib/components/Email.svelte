794c9c1f91588a461a9a67a706abb460ced4ee84
lowizetech <lowize.tech@gmail.com>
2025-10-01 19:46:06 +0800

    Updated content. Styled floating hyperlink email and added href

--- src/lib/components/Email.svelte
+++ src/lib/components/Email.svelte
@@ -1,3 +1,9 @@
-<div>
-	<a href="" title=""></a>
-</div>
+<a
+	href="https://mail.google.com/mail/?view=cm&fs=1&to=lowize.tech@gmail.com&su=PORTFOLIO%20REVIEW&body=Hello%20Louise%2C%0A%0AI%20came%20across%20your%20portfolio%20and%20wanted%20to%20reach%20out.%20I%20was%20interested%20in%20learning%20more%20about%20your%20skills%20and%20experience%2C%20and%20I%20thought%20this%20would%20be%20a%20good%20opportunity%20to%20connect.%0A%0ARegards%2C%0A%5BYour%20Name%5D"
+	title="email"
+	rel="noreferrer noopener"
+	target="_blank"
+	class="fixed z-[3] -rotate-90 -left-[70px] top-1/2 text-neutral-400 hover:text-white tracking-[1px] cursor-pointer duration-200 ease sm:block hidden"
+>
+	lowize.tech@gmail.com
+</a>
