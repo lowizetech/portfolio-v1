1bee2da86a31aa3e76ca7095d088898ad1679116
lowizetech <lowize.tech@gmail.com>
2025-10-02 20:10:20 +0800

    Added configuration. Edited extend for font family, anton and inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,svelte,js,ts}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        anton: ['Anton', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      }
+    }
+  },
   plugins: [],
 }
