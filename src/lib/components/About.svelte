3861137fb277438a19a2385b6df402d838f183b1
lowizetech <lowize.tech@gmail.com>
2025-10-04 20:42:07 +0800

    Updated about. Tweaked marquee in the component

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,33 +1,30 @@
 <script>
-  import { onMount } from "svelte";
-  import { gsap } from "gsap";
-
-  onMount(() => {
-    const marquee = document.querySelector(".marquee");
-    const firstChildWidth = marquee.children[0].offsetWidth;
-
-    gsap.to(marquee, {
-      x: -firstChildWidth,
-      duration: 5,
-      ease: "linear",
-      repeat: -1
-    });
-  });
+	
 </script>
 
-<section id="about" class="h-screen w-full flex items-center justify-center text-white select-none overflow-hidden flex-col gap-3">
-    <div class="marquee w-full whitespace-nowrap flex">
-      <h1 class="bg-zinc-800 font-anton text-[4.5rem] inline-block px-10 tracking-[2px]">
-        STAY TUNED. I WILL BE SHOWING UPDATES SOON!
-      </h1>
-      <h1 class="bg-zinc-800 text-blue-600 font-anton text-[4.5rem] inline-block px-10 tracking-[2px]">
-        STAY TUNED. I WILL BE SHOWING UPDATES SOON!
-      </h1>
+<section id="about" class="h-screen w-full flex items-center text-white">
+	<div class="w-full h-screen flex justify-center items-center select-none overflow-hidden flex-col gap-3">
+        <div class="marquee w-full whitespace-nowrap">
+            <h1 class="bg-zinc-800 font-anton text-[4.5rem] inline-block px-10 tracking-[2px]">STAY TUNED. I WILL BE SHOWING UPDATES SOON!</h1>
+            <h1 class="bg-zinc-800 text-blue-600 font-anton text-[4.5rem] inline-block px-10 tracking-[2px]">STAY TUNED. I WILL BE SHOWING UPDATES SOON!</h1>
+            <h1 class="bg-zinc-800 font-anton text-[4.5rem] inline-block px-10 tracking-[2px]">STAY TUNED. I WILL BE SHOWING UPDATES SOON!</h1>
+            <h1 class="bg-zinc-800 text-blue-600 font-anton text-[4.5rem] inline-block px-10 tracking-[2px]">STAY TUNED. I WILL BE SHOWING UPDATES SOON!</h1>
+        </div>
     </div>
 </section>
 
 <style>
 .marquee {
   display: flex;
+  animation: scrollLeft 30s linear infinite;
+}
+
+@keyframes scrollLeft {
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
 }
 </style>
