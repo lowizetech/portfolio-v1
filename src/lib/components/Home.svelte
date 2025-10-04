0c7b34b1660e18810b67de8a950c6d0a630c1d60
lowizetech <lowize.tech@gmail.com>
2025-10-04 20:15:02 +0800

    Updated home. Tweaked styling and GSAP animation for responsiveness

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -30,15 +30,19 @@
     	{ y: 0, opacity: 1, duration: 0.8, ease: "power2.out" }
     );
 
-    let splitter = new SplitText(".split", {type: "chars"});
-    let chars = splitter.chars;
+    function splitTextAnimation() {
+    	let splitter = new SplitText(".split", {type: "chars"});
+	    let chars = splitter.chars;
 
-    gsap.from(chars, {
-    	yPercent: 100,
-    	duration: 0.7,
-    	stagger: 0.05,
-    	ease: "back.out"
-    });
+	    gsap.from(chars, {
+	    	yPercent: 100,
+	    	duration: 0.7,
+	    	stagger: 0.05,
+	    	ease: "back.out"
+	    });
+    }
+
+    document.fonts.ready.then(splitTextAnimation);
 
   });
 	const subtitle = [
@@ -46,15 +50,17 @@
 	    { title: "2+", description: "Completed Projects"},
 	    { title: "20+", description: "Number of Tools Used"}
 	];
+
+
 </script>
 
 <section id="home" class="h-screen w-full flex flex-col gap-[100px] text-white justify-center sm:px-16 md:px-[140px]">
 	<div class="px-4 space-y-6 md:space-y-9 mt-16 sm:mt-0">
-		<h1 id="slideDiv" class="font-anton text-[4rem] md:text-[5rem] flex flex-col leading-[1]">
+		<h1 id="slideDiv" class="break-all font-anton text-[4rem] md:text-[5rem] flex flex-col leading-[1]">
 			<span class="text-white">TECHNICAL</span>
-			<span class="ml-6 text-blue-600">ASSOCIATE</span>
+			<span class="ml-7 text-blue-600">ASSOCIATE</span>
 		</h1>
-		<p class="goUp overflow-hidden md:text-[1.05rem] font-bold w-[500px] text-neutral-400">I am Louise, A Skilled Technical Associate and Administrative Assistant, and a Software Developer.</p>
+		<p class="goUp break-words  md:text-[1.05rem] font-bold w-fit  md:w-[500px] text-neutral-400">I am Louise, A skilled Technical Associate and a Software Developer helping organizations reach heights.</p>
 		<a
 	    href="https://mail.google.com/mail/?view=cm&fs=1&to=lowize.tech@gmail.com&su=PORTFOLIO%20REVIEW&body=Hello%20Louise%20Czedrian%2C%0A%0AI%20came%20across%20your%20portfolio%20and%20wanted%20to%20reach%20out.%20I%20was%20interested%20in%20learning%20more%20about%20your%20skills%20and%20experience%2C%20and%20I%20thought%20this%20would%20be%20a%20good%20opportunity%20to%20connect.%0A%0ARegards%2C%0A%5BYour%20Name%5D"
 	    title="hire me"
@@ -68,11 +74,11 @@
 
 
 	</div>
-	<div class="flex text-center justify-evenly px-2 md:absolute md:flex-col md:text-right bottom-[70px] right-[50px] gap-6">
+	<div class="flex text-center justify-evenly px-2 md:absolute md:flex-col md:text-right bottom-[70px] right-[50px] gap-3 md:gap-7">
 		{#each subtitle as sub}
 			<div class="slide-item">
 				<h3 class="font-anton text-blue-600 text-[2rem]">{sub.title}</h3>
-				<p class="text-[0.9rem] font-extrabold text-neutral-400">{sub.description}</p>
+				<p class="text-[0.9rem] font-extrabold text-neutral-400 break-words">{sub.description}</p>
 			</div>
 		{/each}
 	</div>
